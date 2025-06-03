2fb199036208c3ed19e9f45d3bf9f1d10bb62fe4
k0rfagen <atopolev2910@gmail.com>
2025-06-03 16:58:25 +0300

    стили работают

--- Sibintek_site/settings.py
+++ Sibintek_site/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
