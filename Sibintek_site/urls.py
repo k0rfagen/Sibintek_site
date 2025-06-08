6193f6f42c54ce516fc3d918802be36fb53ff95d
k0rfagen <atopolev2910@gmail.com>
2025-06-08 23:18:12 +0300

    добавил кучу фоток и сделал норм слайдер

--- Sibintek_site/urls.py
+++ Sibintek_site/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index, documentation_page1, documentation_page2, documentation_page3, crew
+from main.views import (
+    index,
+    documentation_page1,
+    documentation_page2,
+    documentation_page3,
+    crew,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', index, name='index'),
-    path('documentation/1.html', documentation_page1, name='page_1'),
-    path('documentation/2.html', documentation_page2, name='page_2'),
-    path('documentation/3.html', documentation_page3, name='page_3'),
-    path('crew.html', crew, name='crew'),
+    path("", index, name="index"),
+    path("documentation/1.html", documentation_page1, name="page_1"),
+    path("documentation/2.html", documentation_page2, name="page_2"),
+    path("documentation/3.html", documentation_page3, name="page_3"),
+    path("crew.html", crew, name="crew"),
 ]
--- main/urls.py
+++ main/urls.py
@@ -8,11 +8,11 @@ from main import views
 
 
 urlpatterns = [
-    path('', include('main.urls')),
-    path('1.html', views.documentation_page1, name='doc_page')
-    path('2.html', views.documentation_page2, name='doc_page')
-    path('3.html', views.documentation_page3, name='doc_page')
-    path('crew.html', views.crew, name='doc_page')
+    path("", include("main.urls")),
+    path("doc1", views.documentation_page1, name="doc_page"),
+    path("doc2", views.documentation_page2, name="doc_page"),
+    path("doc3", views.documentation_page3, name="doc_page"),
+    path("crew", views.crew, name="doc_page"),
 ]
 
 if settings.DEBUG:
