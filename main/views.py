b498cc9d12989bda8872084103e20b71dc921638
Vadim <vadim.d.odin@gmail.com>
2025-06-08 19:28:50 +0300

    сделал странички с документацией

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', context={})
-
-
-
+def documentation_page1(request):
+    return render(request, 'documentation/1.html')
+def documentation_page2(request):
+    return render(request, 'documentation/2.html')
+def documentation_page3(request):
+    return render(request, 'documentation/3.html')
