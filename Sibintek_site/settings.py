9785bc1d41b935a304fe630e76e4540c387ec3ab
Vadim <vadim.d.odin@gmail.com>
2025-06-11 15:34:33 +0300

    debug true

--- Sibintek_site/settings.py
+++ Sibintek_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tp1vqh!fc86dn=4xw81!mqgr-zz1)(k63%h1y5%!$np$*#gu%-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
